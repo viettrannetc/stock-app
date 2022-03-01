8c7930b310d5bf7a3aac0af51bc8964fe4839413
viettrannetc <50583942+viettrannetc@users.noreply.github.com>
2022-03-01 18:02:04 +0700

    get trading history

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -16,5 +16,9 @@ namespace DotNetCoreSqlDb.Models
         public DbSet<Todo> Todo { get; set; }
         public DbSet<StockSymbol> StockSymbol { get; set; }
         public DbSet<StockSymbolHistory> StockSymbolHistory { get; set; }
+        /// <summary>
+        /// https://api-finance-t19.24hmoney.vn/v1/web/stock/transaction-list-ssi?device_id=web&device_name=INVALID&device_model=Windows+10&network_carrier=INVALID&connection_type=INVALID&os=Chrome&os_version=98.0.4758.102&app_version=INVALID&access_token=INVALID&push_token=INVALID&locale=vi&symbol=VCB&page=1&per_page=2000
+        /// </summary>
+        public DbSet<StockSymbolTradingHistory> StockSymbolTradingHistory { get; set; }
     }
 }
---
+++ Models/Database/StockSymbolTradingHistory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCoreSqlDb.Models
+{
+    [Serializable]
+    public class StockSymbolTradingHistory
+    {
+        public int ID { get; set; }
+        /// <summary>
+        /// Price for this transaction
+        /// </summary>
+        public decimal Price { get; set; }
+        /// <summary>
+        /// Compare with the 1st price in the day (open price)
+        /// </summary>
+        public decimal Change { get; set; }
+        /// <summary>
+        /// Vol for each transaction
+        /// </summary>
+        public decimal Match_qtty { get; set; }
+        /// <summary>
+        /// Total vol from begining of the day until this transaction - it's a sum of all Match_qtty of previous transactions in the day
+        /// </summary>
+        public decimal Total_vol { get; set; }
+        /// <summary>
+        /// sd is sell, bu is buy, unknown
+        /// </summary>
+        public string Side { get; set; }
+        public string StockSymbol { get; set; }
+        /// <summary>
+        /// combination between date & TIME
+        /// </summary>
+        public DateTime Date { get; set; }
+    }
+}
+
